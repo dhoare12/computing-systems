35a5537503aedbecb952c484b0969498f8a4dbe9
David Hoare <d.hoare@marketinvoice.com>
2018-05-11 23:00:19 +0100

    Complete ALU

--- ComputingSystems.BoolArith/ALU/Alu.cs
+++ ComputingSystems.BoolArith/ALU/Alu.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ComputingSystems.CombLogic.ReferenceImplementations;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ComputingSystems.BoolArith.ALU
@@ -11,7 +13,11 @@ namespace ComputingSystems.BoolArith.ALU
 
         private readonly SixteenBitAdder _adder = new SixteenBitAdder();
         private readonly SixteenBitAnder _ander = new SixteenBitAnder();
+
+        private readonly And[] _ands = Enumerable.Range(0, 16).Select(_ => new And()).ToArray();
         private readonly SixteenBitNegator _negator = new SixteenBitNegator();
+        private readonly SixteenBitNegator _negator2 = new SixteenBitNegator();
+
         private readonly SixteenBitMux _mux1 = new SixteenBitMux();
         private readonly SixteenBitMux _mux2 = new SixteenBitMux();
 
@@ -42,11 +48,11 @@ namespace ComputingSystems.BoolArith.ALU
             {
                 _xTransform.Fill(Zx, Nx, X);
                 _yTransform.Fill(Zy, Ny, Y);
-                
+
                 _adder.Fill(_xTransform.Output, _yTransform.Output);
                 _ander.Fill(_xTransform.Output, _yTransform.Output);
                 _mux1.Fill(_adder.Outputs, _ander.Output, F);
-                
+
                 _negator.Fill(_mux1.Output);
                 _mux2.Fill(_mux1.Output, _negator.Output, No);
 
@@ -54,7 +60,27 @@ namespace ComputingSystems.BoolArith.ALU
             }
         }
 
-        public bool Zr { get; }
-        public bool Ng { get; }
+        public bool Zr
+        {
+            get
+            {
+                _negator2.Fill(Out);
+                var output = _negator2.Output;
+                _ands[0].Fill(output[0], output[1]);
+                for (var i = 1; i < 15; i++)
+                {
+                    _ands[i].Fill(_ands[i - 1].Output, output[i + 1]);
+                }
+                return _ands[15].Output;
+            }
+        }
+
+        public bool Ng
+        {
+            get
+            {
+                return Out[0];
+            }
+        }
     }
 }
